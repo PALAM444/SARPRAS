662415c4e79ccc3f40af28d48654063f0e9d75c9
PALAM444 <pcsrjrt4@gmail.com>
2025-05-11 20:42:08 +0700

    Add files via upload

---
+++ rsud/forms.php
@@ -0,0 +1,7 @@
+<input type="text" name="nama_lengkap" value="<?php echo date('d-m-Y'); ?>"    >
+<input type="password" name="password">
+<input type="date" name="tanggal">
+<input type="datetime-local" name="waktu">
+<input type="radio" name="waktu">
+<input type="checkbox" name="waktu">
+<input type="select" name="pilihan1">
